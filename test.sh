837bcbeceedbe0d2459fa4cc6fd44fd98e79b470
Mårten Cassel <marten.cassel@gmail.com>
2025-10-28 23:03:07 +0100

    add testcase for consistency issue on interrupt blob stream

--- test.sh
+++ test.sh
@@ -44,3 +44,18 @@ time podman pull localhost:5000/gitlab/gitlab-org/gitlab-runner:alpine --tls-ver
 podman rmi localhost:5000/redhat/ubi8/ubi-minimal:latest||true
 time podman pull localhost:5000/redhat/ubi8/ubi-minimal:latest --tls-verify=false
 time podman pull localhost:5000/redhat/ubi8/ubi-minimal:latest --tls-verify=false
+
+# Testing consistency during intterruption of pull
+# podman rmi localhost:5000/dockerhub/postgres:latest||true
+# 1. Launch server
+# 2. Start pull: podman image pull  localhost:5000/dockerhub/postgres:latest  --tls-verify=false
+# 3. During pull, kill server, using CTRL+C
+# 4. Restart server
+# 5. Re-run pull command, it should return an error about mismatched digest and not start over
+#
+# Error: writing blob: storing blob to file "/var/tmp/storage948283329/3":
+# happened during read: Digest did not match, expected
+# sha256:23ed4c7c49ef8bac9af6c023d630098a6e6e3e601363ebe7620506124f62df8c,
+# got sha256:33679430848de32c3d5c2ed1d5bd9b71fd3c2113dec30d3d383ecf70c65cdc94
+
+
